ea96602dc473e0a0eea437031ad167437d08c502
andrea-yamileth <andreachaquinga2005@gmail.com>
2025-05-22 17:53:27 -0500

    hola

---
+++ src/main/java/espoch/edu/ec/examenfinciclo/ExamenFinCiclo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package espoch.edu.ec.examenfinciclo;
+
+/**
+ *
+ * @author LAPTOP DELL
+ */
+public class ExamenFinCiclo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
